9510b00bbf45f94abfff0ce541e40cebdcee9558
catRoman-laptop:wsl-ubuntu <catlinroman84@gmail.com>
2024-01-10 10:58:18 -0800

    header file for status lights done

--- main/rgb_led.h
+++ main/rgb_led.h
@@ -8,6 +8,39 @@
 #ifndef MAIN_RGB_LED_H_
 #define MAIN_RGB_LED_H_
 
+// RGB LED GPIO
+#define RGB_LED_RED_GPIO        21
+#define RGB_LED_GREEN_GPIO      20
+#define RGB_LED_BLUE_GPIO       19
+
+// RGB LED color mix channels
+#define RGB_LED_CHANNEL_NUM     3
+
+// RGB LED configuration
+typedef struct
+{
+    int channe;
+    int gpio;
+    int mode;
+    int timeer_index;
+} ledc_info_t;
+
+
+/**
+ * Color to indicate wifi app started
+*/
+void rgb_led_wifi_app_started(void);
+
+/**
+ * Color to indicate http server app started
+*/
+void rgb_led_http_server_started(void);
+
+/**
+ * Color to indicate that ESP32 is connected to an access point
+ * */
+void rgb_led_wifi_connected(void);
+
 
 
 #endif
