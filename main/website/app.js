8c5e2057686119e4a04a0b2be6b2060e1aed161e
catRoman-laptop:wsl-ubuntu <catlinroman84@gmail.com>
2024-01-16 23:42:21 -0800

    updated, index, css, js uri requests and handler for new function

--- main/website/app.js
+++ main/website/app.js
@@ -11,6 +11,7 @@ var wifiConnectInterval = null;
 $(document).ready(function(){
 	getUpdateStatus();
     startDHTSensorInterval();
+    getConnectInfo();
     $("#connect_wifi").on("click", function(){
         checkCredentials();
     });
@@ -178,6 +179,7 @@ function getWifiConnectStatus()
             document.getElementById("wifi_connect_status").innerHTML = 
                 "<h4 class=\"gr\">Connection Successful</h4>";
                 stopWifiConnectStatusInterval();
+                getConnectInfo();
         }
         
     }
@@ -259,4 +261,27 @@ function showPassword()
     {
         x.type = "password";
     }
+}
+
+/**
+ * Gets the connection information for displaying on the web page
+ */
+function getConnectInfo()
+{
+    $.getJSON('/wifiCOnnectInfo.json', function(data)
+    {
+        $("#connected_ap_label").html("Connected to: ");
+        $("#connected_ap").text(data["ap"]);
+
+        $("#ip_address_label").html("IP Address: ");
+        $("#wifi_connect_ip").text(data["ip"]);
+
+        $("#netmask_label").html("Netmask: ");
+        $("#wifi_connect_netmask").text(data["netmask"]);
+
+        $("#gateway_label").html("Gateway: ");
+        $("#wifi_connect_gw").text(data["gw"]);
+
+        document.getElementById('disconnect_wifi').style.display = 'block';
+    })
 }
