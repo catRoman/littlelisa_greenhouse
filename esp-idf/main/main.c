328f8db92bacb43a69dbe6acd90098874cff8345
catRoman_server <catlinroman84@gmail.com>
2024-02-14 18:03:29 -0800

    integrating rtc driver

--- esp-idf/main/main.c
+++ esp-idf/main/main.c
@@ -18,6 +18,7 @@
 #include "sntp.h"
 #include "spi_sd_card.h"
 #include "rtc_DS1302.h"
+#include "ds1302.h"
 
 //TODO implement ntc clock with rtc backup/sync
 //TODO capacicance meter driver
@@ -28,6 +29,25 @@
 
 SemaphoreHandle_t wifiInitSemephore = NULL;
 
+char * binary_string( uint8_t decNum )
+{
+    char * binaryString = malloc(sizeof(char)*11);
+    char * bitString= malloc(sizeof(char)*9);
+
+    int k = 8;
+    for(unsigned int i = 0; i <=8; i++){
+        bitString[--k] = (((decNum >> i) & 1) ? '1' : '0');
+    }
+    bitString[8]='\0';
+    binaryString[0] = '0';
+    binaryString[1]='b';
+    binaryString[2]='\0';
+
+    strcat(binaryString, bitString);
+
+
+    return binaryString;
+}
 
 /**
  * freeRTOS function invocation
@@ -42,6 +62,42 @@ void app_main(void)
 
     //synced system clock
     sntp_service_init();
+
+    
+    ds1302_t clock2 = {
+        .ce_pin = 32, //tested at 19
+        .io_pin = 35, //tested at 18
+        .sclk_pin = 34 //tested at 5
+    };
+
+    ds1302_init(&clock2);
+    ds1302_start(&clock2, false);
+
+    struct tm testTime = {
+        .tm_sec = 15,
+        .tm_min = 05,
+        .tm_hour  = 8,
+        .tm_mday = 28,
+        .tm_mon = 01,
+        .tm_wday = 3,
+        .tm_year = 100
+    };
+    uint8_t val =0;
+    for(int i = 0 ; i < 110; i++){
+        printf("year struct: %s\n", binary_string(testTime.tm_year));
+        printf("test struct time: %s\n", asctime(&testTime));
+        ds1302_set_time(&clock2, &testTime);
+        ds1302_get_time(&clock2, &testTime);
+        printf("\n");
+        printf("struct time after set/get: %s\n", asctime(&testTime));
+        printf("struct year reg after set/get: %s\n", binary_string(testTime.tm_year));
+        read_register(&clock2, RTC_REG_YEAR_READ, &val);
+        printf("year reg val: %s\n", binary_string(val));
+        testTime.tm_year = testTime.tm_year + 1;
+        printf("\n");
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+
     //rtc_DS1302_init();
 
     // Start Wifi
@@ -54,6 +110,8 @@ void app_main(void)
     DHT22_sensor_task_start();
 
     vTaskDelay(15000/ portMAX_DELAY);
+
+
 
 
 }
