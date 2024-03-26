c93750d08f5b66eab851f03bf457d498acafd49a
catRoman-laptop:wsl-ubuntu <catlinroman84@gmail.com>
2024-03-25 21:05:09 -0700

    added components

--- esp-idf/components/database_components/sd_card_db.h
+++
@@ -1,2 +0,0 @@
-void sd_db_task(void *vpParam);
-void sd_db_init(void);
---
+++ esp-idf/components/database_ctrl/include/sd_card_db.h
@@ -0,0 +1,7 @@
+#ifndef DATABASE_CTRL_SD_CARD_DB_H
+#define DATABASE_CTRL_SD_CARD_DB_H
+
+void sd_db_task(void *vpParam);
+void sd_db_init(void);
+
+#endif /*DATABASE_CTRL_SD_CARD_DB_H*/
