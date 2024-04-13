58a34d854ef380816f4a5527ed70865f82102bf4
Cat Roman -Desktop <cjroman@lakeheadu.ca>
2024-04-12 21:21:30 -0700

    device info json and api handler added

--- esp-idf/components/nvs_config/node_info.c
+++ esp-idf/components/nvs_config/node_info.c
@@ -8,11 +8,22 @@
 #include "esp_wifi_types.h"
 #include "esp_wifi.h"
 #include "esp_timer.h"
+#include "esp_system.h"
+#include "esp_app_desc.h"
+#include "esp_chip_info.h"
+#include "esp_idf_version.h"
 
 //components
 #include "node_info.h"
 #include "nvs_service.h"
 #include "sensor_tasks.h"
+
+
+//static protypes
+static char *get_reset_reason(void);
+static char *get_chip_model(esp_chip_info_t *chip_info);
+
+
 
 static const char NODE_INFO_TAG[] = "node_info";
 
@@ -125,7 +136,7 @@ char *node_info_get_module_info_json(void){
         }
     }
        char *json_string = cJSON_Print(root);
- 
+
 
 
 	cJSON_Delete(root);
@@ -202,4 +213,258 @@ char *node_info_get_uptime_json(void){
 	return uptimeFunk;
 
 
+}
+//-------------status
+//sd card
+//sntp
+//ws-server
+//nvs load
+
+char * node_info_get_status_info_json(){
+        cJSON *root = cJSON_CreateObject();
+
+
+  // cJSON_AddStringToObject(root, "reset_reason", get_reset_reason());
+
+
+
+    char *json_string = cJSON_Print(root);
+
+
+	cJSON_Delete(root);
+	return json_string;
+}
+
+
+
+//-------------Device name
+
+
+
+char * node_info_get_device_info_json(){
+    cJSON *root = cJSON_CreateObject();
+
+    //chipinfo
+    esp_chip_info_t this_chip_info;
+    esp_chip_info(&this_chip_info);
+
+    cJSON *chip_info = cJSON_CreateObject();
+    cJSON_AddItemToObject(root, "chip_info", chip_info);
+    cJSON_AddNumberToObject(chip_info, "num_cores", this_chip_info.cores);
+    cJSON_AddStringToObject(chip_info, "chip_type", get_chip_model(&this_chip_info));
+
+    //idf version
+    cJSON_AddStringToObject(root,"idf_ver", esp_get_idf_version());
+
+    //app descriptiong
+    esp_app_desc_t *app_info = esp_app_get_description();
+
+    cJSON *app_desc = cJSON_CreateObject();
+    cJSON_AddItemToObject(root, "app_info", app_desc);
+    cJSON_AddNumberToObject(app_desc, "secure_ver", app_info->secure_version);
+    cJSON_AddStringToObject(app_desc, "app_ver", app_info->version);
+    cJSON_AddStringToObject(app_desc, "proj_name", app_info->project_name);
+    cJSON_AddStringToObject(app_desc, "idf_ver", app_info->idf_ver);
+    cJSON *compile_info = cJSON_CreateObject();
+    cJSON_AddItemToObject(app_desc, "compile_info", compile_info);
+    cJSON_AddStringToObject(compile_info, "time", app_info->time);
+    cJSON_AddStringToObject(compile_info, "date", app_info->date);
+
+
+  //  cJSON_AddStringToObject(root, "reset_reason", get_reset_reason());
+
+
+
+    char *json_string = cJSON_Print(root);
+
+
+	cJSON_Delete(root);
+	return json_string;
+}
+
+
+//------------network information
+//wifi status
+//ip address
+//mac address
+//ssid
+//signal strength
+//connected devices
+//mdns name
+
+char * node_info_get_network_inf_json(){
+
+
+
+   // cJSON_AddStringToObject(root, "reset_reason", get_reset_reason());
+    char ipInfoJSON[200];
+    memset(ipInfoJSON, 0, sizeof(ipInfoJSON));
+
+    char ip[IP4ADDR_STRLEN_MAX];
+    char netmask[IP4ADDR_STRLEN_MAX];
+    char gw[IP4ADDR_STRLEN_MAX];
+
+    wifi_ap_record_t wifi_data;
+    ESP_ERROR_CHECK(esp_wifi_sta_get_ap_info(&wifi_data));
+    char *ssid = (char*)wifi_data.ssid;
+
+    esp_netif_ip_info_t ip_info;
+
+    ESP_ERROR_CHECK(esp_netif_get_ip_info(esp_netif_get_handle_from_ifkey("WIFI_STA_DEF"), &ip_info));
+    esp_ip4addr_ntoa(&ip_info.ip, ip, IP4ADDR_STRLEN_MAX);
+    esp_ip4addr_ntoa(&ip_info.netmask, netmask, IP4ADDR_STRLEN_MAX);
+    esp_ip4addr_ntoa(&ip_info.gw, gw, IP4ADDR_STRLEN_MAX);
+
+    sprintf(ipInfoJSON, "{\"ip\":\"%s\",\"netmask\":\"%s\",\"gw\":\"%s\",\"ap\":\"%s\"}", ip, netmask, gw, ssid);
+
+
+
+
+
+
+	return ipInfoJSON;
+}
+
+//-----------------system health
+//available ram at boot
+
+//memory usage
+//heap memory
+
+
+//heap descirption
+//last reset reason
+
+
+char * node_info_get_system_health_info_json(){
+
+    cJSON *root = cJSON_CreateObject();
+    cJSON *memory_info = cJSON_CreateObject();
+    cJSON *life_cycle = cJSON_CreateObject();
+
+
+
+//memory specific
+    cJSON_AddItemToObject(root, "memory_info", memory_info);
+
+    cJSON_AddNumberToObject(memory_info, "free_heap", esp_get_free_internal_heap_size());
+    cJSON_AddNumberToObject(memory_info, "min_free_heap", esp_get_minimum_free_heap_size());
+
+
+//system life
+    cJSON_AddItemToObject(root, "life_cycle", life_cycle);
+
+
+
+    //uptime
+    cJSON *uptime = cJSON_CreateObject();
+    cJSON_AddItemToObject(life_cycle, "uptime", uptime);
+    cJSON_AddNumberToObject(uptime, "uptime", (esp_timer_get_time() / 1000));
+    cJSON_AddStringToObject(uptime, "unit", "ms");
+
+    //reset-reason
+    cJSON_AddStringToObject(life_cycle, "reset_reason", get_reset_reason());
+
+
+
+    char *json_string = cJSON_Print(root);
+
+
+	cJSON_Delete(root);
+	return json_string;
+}
+
+
+
+
+static char *get_reset_reason(){
+char reset_reason[60];
+
+switch(esp_reset_reason()){
+        case ESP_RST_POWERON:
+            strncpy(reset_reason, "Reset due to power-on event.", sizeof(reset_reason) -1);
+            break;
+
+        case ESP_RST_EXT:
+            strncpy(reset_reason, "Reset by external pin (not applicable for ESP32)", sizeof(reset_reason) -1);
+            break;
+
+        case ESP_RST_SW:
+            strncpy(reset_reason, "Software reset via esp_restart.", sizeof(reset_reason) -1);
+            break;
+
+        case ESP_RST_PANIC:
+            strncpy(reset_reason, "Software reset due to exception/panic.", sizeof(reset_reason) -1);
+            break;
+
+        case ESP_RST_INT_WDT:
+            strncpy(reset_reason, "Reset (software or hardware) due to interrupt watchdog.", sizeof(reset_reason) -1);
+            break;
+
+        case ESP_RST_TASK_WDT:
+            strncpy(reset_reason, "Reset due to task watchdog.", sizeof(reset_reason) -1);
+            break;
+
+        case ESP_RST_WDT:
+            strncpy(reset_reason, "Reset due to other watchdogs.", sizeof(reset_reason) -1);
+            break;
+
+        case ESP_RST_DEEPSLEEP:
+            strncpy(reset_reason, "Reset after exiting deep sleep mode.", sizeof(reset_reason) -1);
+            break;
+
+        case ESP_RST_BROWNOUT:
+            strncpy(reset_reason, "Brownout reset (software or hardware)", sizeof(reset_reason) -1);
+            break;
+
+        case ESP_RST_SDIO:
+            strncpy(reset_reason, "Reset over SDIO.", sizeof(reset_reason) -1);
+            break;
+
+        default:
+            strncpy(reset_reason, "Reset reason can not be determined.", sizeof(reset_reason) -1);
+    }
+
+    reset_reason[sizeof(reset_reason) - 1] = '\0';
+
+    return reset_reason;
+}
+
+static char *get_chip_model(esp_chip_info_t *chip_info){
+char chip_model[15];
+
+
+    switch(chip_info->model){
+
+        case CHIP_ESP32:
+            strncpy(chip_model, "ESP32", sizeof(chip_model) -1);
+
+        case CHIP_ESP32S2:
+            strncpy(chip_model, "ESP32-S2", sizeof(chip_model) -1);
+
+        case CHIP_ESP32S3:
+            strncpy(chip_model, "ESP32-S3", sizeof(chip_model) -1);
+
+        case CHIP_ESP32C3:
+            strncpy(chip_model, "ESP32-C3", sizeof(chip_model) -1);
+
+        case CHIP_ESP32C2:
+            strncpy(chip_model, "ESP32-C2", sizeof(chip_model) -1);
+
+        case CHIP_ESP32C6:
+            strncpy(chip_model, "ESP32-C6", sizeof(chip_model) -1);
+
+        case CHIP_ESP32H2:
+            strncpy(chip_model, "ESP32-H2", sizeof(chip_model) -1);
+
+        case CHIP_ESP32P4:
+            strncpy(chip_model, "ESP32-P4", sizeof(chip_model) -1);
+
+        case CHIP_POSIX_LINUX:
+            strncpy(chip_model, "POSIX/Linux", sizeof(chip_model) -1);
+    }
+
+    chip_model[sizeof(chip_model) - 1] = '\0';
+
+    return chip_model;
 }
