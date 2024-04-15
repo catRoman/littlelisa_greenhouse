2d130a6c53d4721f610a63424a581bcf31359704
Cat Roman -Desktop <cjroman@lakeheadu.ca>
2024-04-15 12:28:37 -0700

    controller client post to nodee server for ota working

--- esp-idf/components/networking/include/http_client.h
+++ esp-idf/components/networking/include/http_client.h
@@ -3,4 +3,6 @@
 
 void post_file_in_chunks(const char *url, const char *file_path);
 esp_err_t do_firmware_upgrade(char *url_str);
+void http_client_get_test(char *url);
+void http_test_task(void *args);
 #endif /*NETWORKING_HTTP_CLIENT_H*/
