7d39525dc6ac77f7c04d688689aa82cad5d53c6f
catRoman-laptop:wsl-ubuntu <catlinroman84@gmail.com>
2024-04-23 15:50:49 -0700

    rtc startup before sntp fix side quest:

--- esp-idf/components/networking/sntp.c
+++ esp-idf/components/networking/sntp.c
@@ -16,8 +16,11 @@
 #include "sntp.h"
 #include "rtc_DS1302.h"
 #include "task_common.h"
+#include "module_config.h"
 
 static const char TAG [] = "sntp";
+extern Module_info_t *module_info_gt;
+
  esp_sntp_config_t config = {
         .smooth_sync = false,
         .server_from_dhcp = false,
@@ -61,71 +64,73 @@ void sntp_service_init(void){
 }
 
 void sntp_server_connection_check_task(void *vpParameter){
+    
+    struct tm time_on_rtc = {0};
 
     setenv("TZ", "UTC+8", 1);
     tzset();
 
-    #ifdef CONFIG_MODULE_TYPE_CONTROLLER
-    bool RTC_IS_ON = true;
+    if(strcmp(module_info_gt->type, "controller") == 0){
+        bool RTC_IS_ON = true;
 
-    ESP_ERROR_CHECK(ds1302_init(&rtc_device));
-    ESP_ERROR_CHECK(ds1302_start(&rtc_device, RTC_IS_ON));
+        ESP_ERROR_CHECK(ds1302_init(&rtc_device));
+        ESP_ERROR_CHECK(ds1302_start(&rtc_device, RTC_IS_ON));
 
-    struct tm time_on_rtc = {0};
-    ESP_ERROR_CHECK(ds1302_get_time(&rtc_device, &time_on_rtc));
-    printf("time from rtc: %s", asctime(&time_on_rtc));
+        ESP_ERROR_CHECK(ds1302_get_time(&rtc_device, &time_on_rtc));
+        printf("time from rtc: %s", asctime(&time_on_rtc));
 
-    time_t rtc_time_sec = mktime(&time_on_rtc);
-    const struct timeval rtc_now = {.tv_sec = rtc_time_sec, .tv_usec = 0};
+        time_t rtc_time_sec = mktime(&time_on_rtc);
+        const struct timeval rtc_now = {.tv_sec = rtc_time_sec, .tv_usec = 0};
 
-    if(settimeofday(&rtc_now, NULL) != 0){
-        ESP_LOGE(TAG, "Failed to set system time from rtc");
-    }else{
-        ESP_LOGI(TAG, "The current date/time on rtc before sync is: %s", ctime(&rtc_time_sec));
+        if(settimeofday(&rtc_now, NULL) != 0){
+            ESP_LOGE(TAG, "Failed to set system time from rtc");
+        }else{
+            ESP_LOGI(TAG, "The current date/time on rtc before sync is: %s", ctime(&rtc_time_sec));
+        }
     }
-    #endif
     bool initial_sync = true;
 
     sntp_set_sync_interval(3600000); //one hour sync
     for(;;){
+        
+        time_t current_time = time(NULL);
 
 
-                if(sntp_get_sync_status() == SNTP_SYNC_STATUS_COMPLETED){
-                    ESP_LOGI(TAG, "sntp sync complete");
-                    ESP_LOGI(TAG, "sync interval set to  %lu", sntp_get_sync_interval());
-                    ESP_LOGI(TAG, "current sntp server set to: %s at %s", esp_sntp_getservername(0), ipaddr_ntoa(esp_sntp_getserver(0)));
-                    //sync with rtc
-                    #ifdef CONFIG_MODULE_TYPE_CONTROLLER
-                    time_t current_time = time(NULL);
+        if(sntp_get_sync_status() == SNTP_SYNC_STATUS_COMPLETED){
+            ESP_LOGI(TAG, "sntp sync complete");
+            ESP_LOGI(TAG, "sync interval set to  %lu", sntp_get_sync_interval());
+            ESP_LOGI(TAG, "current sntp server set to: %s at %s", esp_sntp_getservername(0), ipaddr_ntoa(esp_sntp_getserver(0)));
+            //sync with rtc
+            if(strcmp(module_info_gt->type, "controller") == 0){
 
-                    ESP_LOGI(TAG, "current time set to:  %s", ctime(&current_time));
+                ESP_LOGI(TAG, "current time set to:  %s", ctime(&current_time));
 
-                    ESP_ERROR_CHECK(ds1302_set_write_protect(&rtc_device, false));
-                    ESP_ERROR_CHECK(ds1302_get_time(&rtc_device, &time_on_rtc));
+                ESP_ERROR_CHECK(ds1302_set_write_protect(&rtc_device, false));
+                ESP_ERROR_CHECK(ds1302_get_time(&rtc_device, &time_on_rtc));
 
-                    // ESP_LOGI(TAG, "{==RTC_DS1302==} previous time retrieved from rtc: %s", asctime(&time_on_rtc));
-                    ESP_ERROR_CHECK(ds1302_set_time(&rtc_device, localtime(&current_time)));
+                // ESP_LOGI(TAG, "{==RTC_DS1302==} previous time retrieved from rtc: %s", asctime(&time_on_rtc));
+                ESP_ERROR_CHECK(ds1302_set_time(&rtc_device, localtime(&current_time)));
 
-                    ESP_ERROR_CHECK(ds1302_get_time(&rtc_device, &time_on_rtc));
-                    ESP_LOGI(TAG, "{==RTC_DS1302==} set to time retrieved from sntp service: %s", asctime(&time_on_rtc));
-                    ESP_ERROR_CHECK(ds1302_set_write_protect(&rtc_device, false));
-                    ESP_ERROR_CHECK(ds1302_start(&rtc_device, true));
-                    #endif
-                    initial_sync = false;
+                ESP_ERROR_CHECK(ds1302_get_time(&rtc_device, &time_on_rtc));
+                ESP_LOGI(TAG, "{==RTC_DS1302==} set to time retrieved from sntp service: %s", asctime(&time_on_rtc));
+                ESP_ERROR_CHECK(ds1302_set_write_protect(&rtc_device, false));
+                ESP_ERROR_CHECK(ds1302_start(&rtc_device, true));
+            }
+            initial_sync = false;
 
 
-                }else if (initial_sync == true){
+        }else if (initial_sync == true){
 
-                    ESP_LOGI(TAG, "Waiting for system time to be set for intial sync... ");
-                    vTaskDelay(5000 / portTICK_PERIOD_MS);
-                }
+            ESP_LOGI(TAG, "Waiting for system time to be set for intial sync... ");
+            vTaskDelay(5000 / portTICK_PERIOD_MS);
+        }
 
-                if(initial_sync == false){
+        if(initial_sync == false){
 
-                    vTaskDelay(pdMS_TO_TICKS(360000));
-                }
+            vTaskDelay(pdMS_TO_TICKS(360000));
+        }
 
-                // TODO: set syncing task
+        // TODO: set syncing task
 
 
     }
