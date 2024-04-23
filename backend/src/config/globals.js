f990383b518a9895f4c61820e80b8c593ab2b316
Cat Roman -Desktop <cjroman@lakeheadu.ca>
2024-04-23 11:11:40 -0700

    http client wrote for sending sensor data to express

--- backend/src/config/globals.js
+++ backend/src/config/globals.js
@@ -1,18 +1,12 @@
-import { dirname, join } from 'path';
-import { fileURLToPath } from 'url';
-
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
 
 const Web_Config_g = {
-    PORT: 3000,
-    SERVER_IP: '10.0.0.204'
-
-}
+  PORT: 3000,
+  SERVER_IP: "10.0.0.53",
+};
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-const __root_dir = join(__dirname, '../../..')
+const __root_dir = join(__dirname, "../../..");
 
-export {
-    Web_Config_g,
-    __dirname,
-    __root_dir
-};
+export { Web_Config_g, __dirname, __root_dir };
