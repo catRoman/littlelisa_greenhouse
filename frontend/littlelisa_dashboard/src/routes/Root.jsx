3b075277cd69ca7b997417d2e436697ad0ca0467
Cat Roman -Desktop <cjroman@lakeheadu.ca>
2024-04-26 13:35:57 -0700

    starting dashboard router

---
+++ frontend/littlelisa_dashboard/src/routes/Root.jsx
@@ -0,0 +1,12 @@
+export default function Root() {
+  return (
+    <>
+      <ul>
+        <li>DASHBOARD HOME</li>
+        <li>SENSOR HISTORY</li>
+        <li>ZONES</li>
+        <li>SETTINGS</li>
+      </ul>
+    </>
+  );
+}
