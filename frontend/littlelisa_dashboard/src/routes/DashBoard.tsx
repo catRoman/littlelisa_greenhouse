7a7063c8207b8dd810f45f301eca47136f92bf2f
catRoman-laptop:wsl-ubuntu <catlinroman84@gmail.com>
2024-04-26 16:48:30 -0700

    moving on

---
+++ frontend/littlelisa_dashboard/src/routes/DashBoard.tsx
@@ -0,0 +1,13 @@
+export default function DashBoard() {
+  return (
+    <main className="m-4">
+      <h1>hello im a page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus fugiat
+        saepe id aspernatur officia recusandae cumque in quia officiis?
+        Laboriosam eum debitis incidunt voluptates temporibus distinctio velit,
+        voluptatum accusamus? Doloribus.
+      </p>
+    </main>
+  );
+}
