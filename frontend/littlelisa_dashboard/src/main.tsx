255925f285339e24d6a55b52c25c866279a5d64f
catRoman-laptop:wsl-ubuntu <catlinroman84@gmail.com>
2024-04-26 14:50:43 -0700

    setting up project tailwind

--- frontend/littlelisa_dashboard/src/main.tsx
+++ frontend/littlelisa_dashboard/src/main.tsx
@@ -1,20 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Root from "./routes/Root.jsx";
+import Root from "./routes/Root.tsx";
 import App from "./App.tsx";
 import "./index.css";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./ErrorPage.tsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    errorElement: <ErrorPage />,
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
