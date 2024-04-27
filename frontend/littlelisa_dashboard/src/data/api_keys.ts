5c64544dae66f614cabe965632d08f675de10b5d
Cat Roman -Desktop <cjroman@lakeheadu.ca>
2024-04-27 01:38:00 -0700

    wow battle with typescript with key index signatures jesus

---
+++ frontend/littlelisa_dashboard/src/data/api_keys.ts
@@ -0,0 +1,4 @@
+//the noun project apr 26/2024
+export const NOUN_API_KEY = "37c0fae84a0749898605fc0813a0f335";
+//weatherapi.com apr 26/2024
+export const WEATHER_API_KEY = "96315d212b3c425aac312511242704";
