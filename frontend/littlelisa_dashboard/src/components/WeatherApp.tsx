669ffa8a815426ae01cb745d545c5c9c333f4ef0
Cat Roman -Desktop <cjroman@lakeheadu.ca>
2024-04-26 20:24:10 -0700

    writing weatehr component

--- frontend/littlelisa_dashboard/src/components/WeatherApp.tsx
+++ frontend/littlelisa_dashboard/src/components/WeatherApp.tsx
@@ -1,4 +1,14 @@
+import { useEffect, useState } from "react";
+import { fetchWeatherData, type WeatherData } from "../services/currentWeather";
+
 export default function WeatherAp() {
+  const [currentWeather, setCurrentWeather] = useState<WeatherData>();
+
+  useEffect(() => {
+    setInter
+    const weatherData = fetchWeatherData();
+  }, []);
+
   return (
     <aside>
       <h3>Current Weather</h3>
--- frontend/littlelisa_dashboard/src/services/currentWeather.ts
+++ frontend/littlelisa_dashboard/src/services/currentWeather.ts
@@ -1,4 +1,32 @@
-//weatherapi.com
-const weatherApiKey = '96315d212b3c425aac312511242704';
+//weatherapi.com apr 26/2024
+const weatherApiKey = "96315d212b3c425aac312511242704";
 
-const currentWeatherApiUrl = 'http://api.weatherapi.com/v1/current.json?key=96315d212b3c425aac312511242704&q=48.42,-123.53&aqi=no';
+const currentWeatherApiUrl = `http://api.weatherapi.com/v1/current.json?key=${weatherApiKey}{&q=48.42,-123.53&aqi=no`;
+
+export interface WeatherData {
+  temp_c: number;
+  weather_code: number;
+  wind_kph: number;
+  percip_mm: number;
+  humidity: number;
+  feelslike_c: number;
+}
+
+export async function fetchWeatherData(): Promise<WeatherData | null> {
+  try {
+    const response = await fetch(currentWeatherApiUrl);
+    if (!response.ok) {
+      throw new Error("Failed to fetch WeatherData");
+    }
+
+    const {
+      condition: { code: weather_code },
+      ...other
+    } = await response.json();
+
+    return { weather_code, ...other };
+  } catch (error) {
+    console.log("Error requesting current weather data", error);
+    return null;
+  }
+}
