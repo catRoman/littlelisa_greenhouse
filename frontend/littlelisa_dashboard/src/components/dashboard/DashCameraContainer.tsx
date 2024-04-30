79c85575dd5430c8acd6e4509471f3a32f49074f
Cat Roman -Desktop <cjroman@lakeheadu.ca>
2024-04-30 00:09:13 -0700

    the forgoten ones...

--- frontend/littlelisa_dashboard/src/components/dashboard/DashCameraContainer.tsx
+++ frontend/littlelisa_dashboard/src/components/dashboard/DashCameraContainer.tsx
@@ -3,7 +3,7 @@ export default function DashCameraContainer() {
     <div className="aspect-w-16 aspect-h-9 relative overflow-hidden rounded-lg border-4 border-solid border-stone-500">
       <img
         className="h-full w-full object-cover"
-        src="http://10.0.0.249/camStream"
+        src="http://localhost:3000/camStream"
         width="400"
       />
     </div>
