92b85db0f035893c06fbc3d18d140fa4e76ba904
Cat Roman -Desktop <cjroman@lakeheadu.ca>
2024-05-10 22:11:48 -0700

    added api key

--- frontend/littlelisa_dashboard/src/data/api_keys.ts
+++ frontend/littlelisa_dashboard/src/data/api_keys.ts
@@ -3,4 +3,6 @@ export const NOUN_API_KEY = "37c0fae84a0749898605fc0813a0f335";
 //weatherapi.com apr 26/2024
 export const WEATHER_API_KEY = "96315d212b3c425aac312511242704";
 //trefle.io -- plant db api may 10/2024
-export const TREFLE_KEY = "nKnE3GL83YDnuhFSzMBfztaCL6H8GmDV6V8FOQnE5k0";
+export const TREFLE_API_KEY = "nKnE3GL83YDnuhFSzMBfztaCL6H8GmDV6V8FOQnE5k0";
+//perenial.com -- plant db may10/2024
+export const PERENUAL_API_KEY = "sk-D9Cz663efbf3a0d805421";
