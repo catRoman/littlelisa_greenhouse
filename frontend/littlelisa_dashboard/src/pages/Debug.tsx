461fd680afa0b84ce2ace036417d847f91417522
catRoman-laptop:wsl-ubuntu <catlinroman84@gmail.com>
2024-05-11 02:22:54 -0700

    removed old three component gui tools kept getting loaded

--- frontend/littlelisa_dashboard/src/pages/Debug.tsx
+++ frontend/littlelisa_dashboard/src/pages/Debug.tsx
@@ -4,9 +4,7 @@ export default function Debug() {
   return (
     <div className="pr-6">
       <h1>Debug page</h1>
-      <div>
-        <GreenHouseRender cssClass="h-80" />
-      </div>
+      <div>{/* <GreenHouseRender cssClass="h-80" /> */}</div>
     </div>
   );
 }
