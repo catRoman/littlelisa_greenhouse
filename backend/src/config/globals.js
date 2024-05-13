65823b068cb3a7cceb48120d9bee014a08a4ac9c
catRoman-laptop:wsl-ubuntu <catlinroman84@gmail.com>
2024-05-13 14:48:39 -0700

    adding uris for when working on laptop

--- backend/src/config/globals.js
+++ backend/src/config/globals.js
@@ -3,7 +3,10 @@ import { fileURLToPath } from "url";
 
 const Web_Config_g = {
   PORT: 3000,
-  SERVER_IP: "10.0.0.53",
+  //-->garage ethernet
+  ///SERVER_IP: "10.0.0.53",
+  //-->laptop wsl
+  SERVER_IP: "172.30.58.251",
 };
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
