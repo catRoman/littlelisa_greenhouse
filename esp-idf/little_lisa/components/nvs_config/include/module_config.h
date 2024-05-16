5a83ba558a979236efe7a77ae3d2eb0a777bf02c
Cat Roman -Desktop <cjroman@lakeheadu.ca>
2024-05-15 17:59:55 -0700

    added greenhouse_id, zone_id , square_pos(row/col)/zn_rel_pos to nvs+module_info meta data with kconfig selectability

--- esp-idf/little_lisa/components/nvs_config/include/module_config.h
+++ esp-idf/little_lisa/components/nvs_config/include/module_config.h
@@ -5,21 +5,26 @@
 
 #include "sensor_tasks.h"
 
-typedef struct Module_sensor_config_t{
+typedef struct Module_sensor_config_t
+{
     char **sensor_loc_arr;
     int8_t *sensor_pin_arr;
     int8_t total_sensor;
-}Module_sensor_config_t;
+} Module_sensor_config_t;
 
-typedef struct Module_info_t{
+typedef struct Module_info_t
+{
     char *type;
     char *location;
     char *identity;
+    int8_t greenhouse_id;
+    int8_t zone_id;
+    int8_t zn_rel_pos[3];
+    int8_t square_pos[2];
     int8_t *sensor_arr;
     Module_sensor_config_t **sensor_config_arr;
 
-}Module_info_t;
-
+} Module_info_t;
 
 esp_err_t initiate_sensor_tasks(void);
 
@@ -30,9 +35,13 @@ Module_sensor_config_t *createModuleSensorConfig(char **locations, int8_t *pins,
 void freeModuleSensorConfig(Module_sensor_config_t *config);
 
 Module_info_t *create_module_from_config(char *type,
-        char *location,
-        int8_t *sensor_arr,
-        Module_sensor_config_t **sensor_config_arr);
+                                         int8_t greenhouse_id,
+                                         int8_t zone_id,
+                                         int8_t zn_rel_pos[3],
+                                         int8_t square_pos[2],
+                                         char *location,
+                                         int8_t *sensor_arr,
+                                         Module_sensor_config_t **sensor_config_arr);
 
 Module_info_t *create_module_from_NVS(void);
 
