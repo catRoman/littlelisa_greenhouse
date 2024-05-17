1b5d7cabbe86dbf5c7617c21bd5e0267b3e4be49
Cat Roman -Desktop <cjroman@lakeheadu.ca>
2024-05-17 05:52:20 -0700

    sql functions working -> sensor data is properly being assigned to square or rel_zn_pos :-)

--- database/local-proto/api-functions/helper-functions/sensor_data_from_controller.sql
+++ database/local-proto/api-functions/helper-functions/sensor_data_from_controller.sql
@@ -8,6 +8,7 @@ declare
     --greenhouse info
     gh_id int;
     z_id int;
+    z_num int;
     --module info
     c_id varchar(25);
     m_type varchar(25);
@@ -17,12 +18,22 @@ declare
     id varchar(25);
     m_loc varchar(50);
     l_s_id int;
+    m_pos_x int;
+    m_pos_y int;
+    m_zn_rel_pos_x int;
+    m_zn_rel_pos_y int;
+    m_zn_rel_pos_z int;
     --sensor info
     s_pin int;
     s_type varchar(25);
     s_ts_str varchar(30);
     s_ts timestamp;
     s_loc varchar(50);
+    s_pos_x int;
+    s_pos_y int;
+    s_zn_rel_pos_x int;
+    s_zn_rel_pos_y int;
+    s_zn_rel_pos_z int;
 
 
 
@@ -31,16 +42,24 @@ declare
     con_id int;
     s_type_id int;
     s_id int;
+    m_selected_square_id int;
+    m_selected_zn_rel_pos_id int;
+    s_selected_square_id int;
+    s_selected_zn_rel_pos_id int;
+    module_square_pos boolean;
+    sensor_square_pos boolean;
 begin
     --extract data fron jsonb
     --greenhouse info
     gh_id := sensor_data->'greenhouse_info'->>'greenhouse_id';
-    z_id  :=sensor_data->'greenhouse_info'->>'zone_id';
+    z_num  :=sensor_data->'greenhouse_info'->>'zone_num';
     --module info
     c_id  :=sensor_data->'module_info'->>'controller_identifier';
     f_ver  :=sensor_data->'module_info'->>'firmware_version';
     m_type :=sensor_data->'module_info'->>'type';
     d_comp_str  :=sensor_data->'module_info'->>'date_compilied';
+
+
         --adjsut timestamp for postgres str exp. "Apr 25 2024 01:12:09"
     d_comp  := to_timestamp(d_comp_str, 'Mon DD YYYY HH24:MI:SS');
     id  :=sensor_data->'module_info'->>'identifier';
@@ -54,7 +73,51 @@ begin
     s_ts := to_timestamp(s_ts_str, 'Dy Mon DD HH24:MI:SS YYYY');
     s_loc  :=sensor_data->'sensor_info'->>'location';
 
-    --variable sensor data
+    ------------------------------------
+    --check data
+    --for moduel
+
+    IF (sensor_data -> 'module_info' -> 'square_pos') ? 'x' THEN
+        m_pos_x := (sensor_data -> 'module_info' -> 'square_pos') ->> 'x';
+        m_pos_y := (sensor_data -> 'module_info' -> 'square_pos') ->> 'y';
+            m_zn_rel_pos_x := NULL;
+        m_zn_rel_pos_y := NULL;
+        m_zn_rel_pos_z := NULL;
+        module_square_pos := true;
+    ELSE
+        m_pos_x := NULL;
+        m_pos_y := NULL;
+        m_zn_rel_pos_x := (sensor_data -> 'module_info' -> 'zn_rel_pos') ->> 'x';
+        m_zn_rel_pos_y := (sensor_data -> 'module_info' -> 'zn_rel_pos') ->> 'y';
+        m_zn_rel_pos_z := (sensor_data -> 'module_info' -> 'zn_rel_pos') ->> 'z';
+        module_square_pos := false;
+    END IF;
+
+
+
+    --for sensor
+    IF (sensor_data -> 'sensor_info' -> 'square_pos') ? 'x' THEN
+        s_pos_x := (sensor_data -> 'sensor_info' -> 'square_pos') ->> 'x';
+        s_pos_y := (sensor_data -> 'sensor_info' -> 'square_pos') ->> 'y';
+                s_zn_rel_pos_x := NULL;
+        s_zn_rel_pos_y := NULL;
+        s_zn_rel_pos_z := NULL;
+        sensor_square_pos := true;
+    ELSE
+        s_pos_x := NULL;
+        s_pos_y := NULL;
+        s_zn_rel_pos_x := (sensor_data -> 'sensor_info' -> 'zn_rel_pos') ->> 'x';
+        s_zn_rel_pos_y := (sensor_data -> 'sensor_info' -> 'zn_rel_pos') ->> 'y';
+        s_zn_rel_pos_z := (sensor_data -> 'sensor_info' -> 'zn_rel_pos') ->> 'z';
+        sensor_square_pos := false;
+    END IF;
+
+
+  m_selected_square_id := null;
+    m_selected_zn_rel_pos_id := null;
+    s_selected_square_id := null;
+    s_selected_zn_rel_pos_id := null;
+    -------------------------------
 
 
 
@@ -64,9 +127,69 @@ begin
     then raise exception 'Greenhouse % not found, rollingback data insert...', gh_id;
     end if;
 
-    if not exists ( select 1 from zones where zone_id = z_id)
-    then raise exception 'Zone % not found, rollingback data insert...', z_id;
+    if not exists ( select 1 from zones where zone_number = z_num)
+    then raise exception 'Zone % not found, rollingback data insert...', z_num;
+    else
+        SELECT zone_id INTO z_id FROM zones WHERE zone_number = z_num;
     end if;
+
+--get the square/zn_rel ids for module and sensor
+    if module_square_pos is true
+    then
+        select square_id into m_selected_square_id from squares
+        where x_pos = m_pos_x and y_pos = m_pos_y;
+    else
+        if not exists (
+            select 1 from zn_rel_pos
+            where
+                x_pos = m_zn_rel_pos_x and
+                y_pos = m_zn_rel_pos_y and
+                z_pos = m_zn_rel_pos_z
+                )
+        then
+            insert into zn_rel_pos(zone_id, x_pos, y_pos, z_pos)
+            values (z_id, m_zn_rel_pos_x ,m_zn_rel_pos_y ,m_zn_rel_pos_z)
+            returning zn_rel_pos_id into m_selected_zn_rel_pos_id;
+        else
+            SELECT zn_rel_pos_id INTO m_selected_zn_rel_pos_id  -- Use SELECT to get the existing ID
+            FROM zn_rel_pos
+            WHERE
+                x_pos = m_zn_rel_pos_x AND
+                y_pos = m_zn_rel_pos_y AND
+                z_pos = m_zn_rel_pos_z;
+
+        end if;
+    end if;
+
+  if sensor_square_pos is true
+    then
+        select square_id into s_selected_square_id from squares
+        where x_pos = s_pos_x and y_pos = s_pos_y;
+    else
+        if not exists (
+            select 1 from zn_rel_pos
+            where
+                x_pos = s_zn_rel_pos_x and
+                y_pos = s_zn_rel_pos_y and
+                z_pos = s_zn_rel_pos_z
+                )
+        then
+            insert into zn_rel_pos(zone_id, x_pos, y_pos, z_pos)
+            values (z_id, s_zn_rel_pos_x ,s_zn_rel_pos_y ,s_zn_rel_pos_z)
+            returning zn_rel_pos_id into s_selected_zn_rel_pos_id;
+        else
+            SELECT zn_rel_pos_id INTO s_selected_zn_rel_pos_id  -- Use SELECT to get the existing ID
+            FROM zn_rel_pos
+            WHERE
+                x_pos = s_zn_rel_pos_x AND
+                y_pos = s_zn_rel_pos_y AND
+                z_pos = s_zn_rel_pos_z;
+
+        end if;
+    end if;
+
+
+
 
     --check if module exist create/update
     if exists (
@@ -81,7 +204,9 @@ begin
             zone_id = z_id,
             firmware_version = f_ver,
             date_compilied = d_comp,
-            location = m_loc
+            location = m_loc,
+            square_id = m_selected_square_id,
+            zn_rel_pos_id = m_selected_zn_rel_pos_id
         where lower(identifier) = lower(id)
         and (
             greenhouse_id != gh_id or
@@ -90,15 +215,28 @@ begin
             date_compilied != d_comp or
             location != m_loc
             );
+
+
     else
         -- module not found
-        insert into modules (greenhouse_id, zone_id, identifier, firmware_version, date_compilied, location )
-        values (gh_id, z_id, id, f_ver, d_comp, m_loc);
+        insert into modules (greenhouse_id, zone_id, identifier, firmware_version, date_compilied, location , square_id, zn_rel_pos_id)
+        values (gh_id, z_id, id, f_ver, d_comp, m_loc, m_selected_square_id, m_selected_zn_rel_pos_id);
+
     end if;
+
 
     --get module_id of current module created/updated
     SELECT module_id INTO mod_id FROM modules WHERE identifier = id;
 
+--set the square/zn_rel position
+    -- if module_square_pos
+    --     then
+    --         update modules set square_id = m_selected_square_id, zn_rel_pos_id = null
+    --         where module_id = mod_id;
+    --     else
+    --         update modules set zn_rel_pos_id = m_selected_zn_rel_pos, square_id = null
+    --         where module_id = mod_id;
+    --     end if;
     --create/update node/controller table based on type
     if lower(m_type) = 'controller'
     then
@@ -170,24 +308,35 @@ begin
         set
             sensor_pin = s_pin,
             location = s_loc,
-            type_id = s_type_id
+            type_id = s_type_id,
+              square_id = s_selected_square_id,
+            zn_rel_pos_id = s_selected_zn_rel_pos_id
         where local_sensor_id = l_s_id
         and module_id = mod_id
         returning sensor_id into s_id;
 
         if not found then  -- No rows updated, so perform the insert instead
-            insert into sensors (module_id, local_sensor_id, sensor_pin, location, type_id)
-            values (mod_id, l_s_id, s_pin, s_loc, s_type_id)
+            insert into sensors (module_id, local_sensor_id, sensor_pin, location, type_id, square_id, zn_rel_pos_id)
+            values (mod_id, l_s_id, s_pin, s_loc, s_type_id, s_selected_square_id, s_selected_zn_rel_pos_id)
             RETURNING sensor_id into s_id;
         end if;
 
     else
         -- module not found
-        insert into sensors (module_id, local_sensor_id, sensor_pin, location, type_id )
-        values (mod_id, l_s_id, s_pin, s_loc, s_type_id)
+        insert into sensors (module_id, local_sensor_id, sensor_pin, location, type_id, square_id, zn_rel_pos_id)
+        values (mod_id, l_s_id, s_pin, s_loc, s_type_id, s_selected_square_id, s_selected_zn_rel_pos_id)
         returning sensor_id into s_id;
     end if;
 
+    --set the square/zn_rel position
+    -- if sensor_square_pos
+    --     then
+    --         update sensors set square_id = s_selected_square_id, zn_rel_pos_id = null
+    --         where sensor_id = s_id;
+    --     else
+    --         update sensors set zn_rel_pos_id = s_selected_zn_rel_pos, square_id = null
+    --         where sensor_id = s_id;
+    --     end if;
     --add sensor data to corresponding inherited sensor_data type based on type
 
     if s_type = 'DHT22'
@@ -203,5 +352,12 @@ begin
     end if;
 
     raise notice 'sensor data added at %', now();
+
+
+
+
+
+
+
 end;
 $$;
