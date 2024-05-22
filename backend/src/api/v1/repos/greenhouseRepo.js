21319e22e780ef9c1da539371cfe01aa36e4fb41
Cat Roman -Desktop <cjroman@lakeheadu.ca>
2024-05-22 13:38:25 -0700

    working through the api

--- backend/src/api/v1/repos/greenhouseRepo.js
+++ backend/src/api/v1/repos/greenhouseRepo.js
@@ -1,7 +1,52 @@
 import BaseRepo from "./baseRepo.js";
+import fs from "fs/promises";
+
 class GreenHousesRepo extends BaseRepo {
   constructor() {
     super("greenhouses", "user");
+  }
+
+  async getTotalZones(greenhouseId) {
+    const query = `
+      Select count(*) as zones
+        from zones
+      where greenhouse_id = $1`;
+    const results = await this.query(query, [greenhouseId]);
+    results[0].zones = Number(results[0].zones);
+    return results[0];
+  }
+  async getTotalControllers(greenhouseId) {
+    const query = `
+      select count(module_id) as controllers from modules
+      join controllers c
+      on
+        c.controller_id = module_id
+      where greenhouse_id = $1`;
+    const results = await this.query(query, [greenhouseId]);
+    results[0].controllers = Number(results[0].controllers);
+    return results[0];
+  }
+  async getTotalNodes(greenhouseId) {
+    const query = `
+    select count(module_id) as nodes
+      from modules
+    join nodes n
+      on n.node_id = module_id
+    where greenhouse_id = $1;`;
+    const results = await this.query(query, [greenhouseId]);
+    results[0].nodes = Number(results[0].nodes);
+    return results[0];
+  }
+  async getTotalSensors(greenhouseId) {
+    const query = `
+    select count(s.module_id) as sensors
+      from sensors s
+    join modules m
+      on m.module_id = s.module_id
+    where m.greenhouse_id = $1;`;
+    const results = await this.query(query, [greenhouseId]);
+    results[0].sensors = Number(results[0].sensors);
+    return results[0];
   }
 }
 
--- backend/src/api/v1/services/greenhouseService.js
+++ backend/src/api/v1/services/greenhouseService.js
@@ -1,4 +1,5 @@
 import GreenHousesRepo from "../repos/greenhouseRepo.js";
+import ControllersRepo from "../repos/controllerRepo.js";
 
 const getAllGreenhouses = async (userId) => {
   const greenhouses = await GreenHousesRepo.getAllByParentId(userId);
@@ -7,7 +8,51 @@ const getAllGreenhouses = async (userId) => {
 
 const getGreenhouseById = async (userId, greenhouseId) => {
   const greenhouse = await GreenHousesRepo.getById(userId, greenhouseId);
-  return greenhouse || null;
+  //aggregates
+  const total_zones = await GreenHousesRepo.getTotalZones(greenhouseId);
+  const total_controllers = await GreenHousesRepo.getTotalControllers(
+    greenhouseId
+  );
+  const total_nodes = await GreenHousesRepo.getTotalNodes(greenhouseId);
+  const total_sensors = await GreenHousesRepo.getTotalSensors(greenhouseId);
+  //controller info
+  const controllers = await ControllersRepo.getAllGreenhouseControllersInfo(
+    greenhouseId
+  );
+
+  //json formatin
+  const x_length = greenhouse.x_length;
+  const y_length = greenhouse.y_length;
+  const z_length = greenhouse.z_length;
+  delete greenhouse.x_length;
+  delete greenhouse.y_length;
+  delete greenhouse.z_length;
+
+  const dimensions = {
+    dimensions: {
+      x: x_length,
+      y: y_length,
+      z: z_length,
+    },
+  };
+
+  const totals = {
+    total: {
+      ...total_zones,
+      ...total_controllers,
+      ...total_nodes,
+      ...total_sensors,
+      ...controllers,
+    },
+  };
+
+  return (
+    {
+      ...greenhouse,
+      ...dimensions,
+      ...totals,
+    } || null
+  );
 };
 
 export default {
