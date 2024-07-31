142380b7eb5b93c9c872af5ddde1c038885e208c
Cat Roman -Desktop <cjroman@lakeheadu.ca>
2024-07-31 00:49:43 -0700

    finishing merge geeeesus

--- backend/src/api/v1/controllers/envStateController.js
+++ backend/src/api/v1/controllers/envStateController.js
@@ -1,15 +1,8 @@
 import eventLogRepo from "../repos/eventLogRepo.js";
 import envStateService from "../services/envStateService.js";
-<<<<<<< HEAD
-import fetch from 'node-fetch'
-const getEnvState = async (req, res) => {
-  try {
-
-=======
 import fetch from "node-fetch";
 const getEnvState = async (req, res) => {
   try {
->>>>>>> landing_page
     const envState = await envStateService.getEnvState();
     console.log(`requested: ${req.originalUrl}`);
 
@@ -21,46 +14,6 @@ const getEnvState = async (req, res) => {
 
 const updateEnvState = async (req, res) => {
   try {
-<<<<<<< HEAD
-
-
-    console.log(`requested: ${req.originalUrl}`);
-
-    const id = req.body;
-    const greenhouseId = req.params.greenhouseId
-
-    const forwardPut = async ()=>{
-
-      try {
-        const response = await fetch('http://10.0.0.86/api/envStateUpdate',{
-          method: 'PUT',
-          body: id
-        });
-        const result = await response.json();
-        console.log(result)
-        if(response.ok){
-             //PARAMS:
-        // eventType,
-        // eventAction,
-        // details,
-        // greenhouseId,
-        // zoneId,
-        // squareId,
-        // moduleId,
-        // sensorId,
-        // note_id
-          const stateObject = Object.values(result);
-          console.log(Number(id))
-          console.log(stateObject)
-
-
-          const selectedState = stateObject.find(state=> state.id === Number(id));
-          console.log(selectedState)
-          const addEvent = await eventLogRepo.addEvent(
-            selectedState.type,
-            selectedState.state,
-            '',
-=======
     console.log(`requested: ${req.originalUrl}`);
 
     const id = req.body;
@@ -97,25 +50,12 @@ const updateEnvState = async (req, res) => {
             selectedState.type,
             selectedState.state,
             "",
->>>>>>> landing_page
             greenhouseId,
             null,
             null,
             null,
             null,
             null
-<<<<<<< HEAD
-          )
-        }
-        return result;
-      } catch (error) {
-        console.error('Error forwarding:', error);
-      }
-    }
-    const forwardedResponse = await forwardPut();
-
-
-=======
           );
         }
         return result;
@@ -125,22 +65,13 @@ const updateEnvState = async (req, res) => {
     };
     const forwardedResponse = await forwardPut();
 
->>>>>>> landing_page
     res.json(forwardedResponse);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 };
 
-<<<<<<< HEAD
-
-
-export default {
-  updateEnvState,
-    getEnvState
-=======
 export default {
   updateEnvState,
   getEnvState,
->>>>>>> landing_page
 };
