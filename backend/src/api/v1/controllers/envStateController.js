b2c993203b3afa286d21005aa0db6d0591b2896b
Cat Roman -Desktop <cjroman@lakeheadu.ca>
2024-07-31 00:17:38 -0700

    old atiny commit, ackend ip point to new controller

--- backend/src/api/v1/controllers/envStateController.js
+++ backend/src/api/v1/controllers/envStateController.js
@@ -1,9 +1,8 @@
 import eventLogRepo from "../repos/eventLogRepo.js";
 import envStateService from "../services/envStateService.js";
-import fetch from 'node-fetch'
+import fetch from "node-fetch";
 const getEnvState = async (req, res) => {
   try {
-
     const envState = await envStateService.getEnvState();
     console.log(`requested: ${req.originalUrl}`);
 
@@ -15,59 +14,56 @@ const getEnvState = async (req, res) => {
 
 const updateEnvState = async (req, res) => {
   try {
-
-
     console.log(`requested: ${req.originalUrl}`);
 
     const id = req.body;
-    const greenhouseId = req.params.greenhouseId
+    const greenhouseId = req.params.greenhouseId;
 
-    const forwardPut = async ()=>{
-
+    const forwardPut = async () => {
       try {
-        const response = await fetch('http://10.0.0.86/api/envStateUpdate',{
-          method: 'PUT',
-          body: id
+        const response = await fetch("http://10.0.0.41/api/envStateUpdate", {
+          method: "PUT",
+          body: id,
         });
         const result = await response.json();
-        console.log(result)
-        if(response.ok){
-             //PARAMS:
-        // eventType,
-        // eventAction,
-        // details,
-        // greenhouseId,
-        // zoneId,
-        // squareId,
-        // moduleId,
-        // sensorId,
-        // note_id
+        console.log(result);
+        if (response.ok) {
+          //PARAMS:
+          // eventType,
+          // eventAction,
+          // details,
+          // greenhouseId,
+          // zoneId,
+          // squareId,
+          // moduleId,
+          // sensorId,
+          // note_id
           const stateObject = Object.values(result);
-          console.log(Number(id))
-          console.log(stateObject)
+          console.log(Number(id));
+          console.log(stateObject);
 
-
-          const selectedState = stateObject.find(state=> state.id === Number(id));
-          console.log(selectedState)
+          const selectedState = stateObject.find(
+            (state) => state.id === Number(id)
+          );
+          console.log(selectedState);
           const addEvent = await eventLogRepo.addEvent(
             selectedState.type,
             selectedState.state,
-            '',
+            "",
             greenhouseId,
             null,
             null,
             null,
             null,
             null
-          )
+          );
         }
         return result;
       } catch (error) {
-        console.error('Error forwarding:', error);
+        console.error("Error forwarding:", error);
       }
-    }
+    };
     const forwardedResponse = await forwardPut();
-
 
     res.json(forwardedResponse);
   } catch (error) {
@@ -75,9 +71,7 @@ const updateEnvState = async (req, res) => {
   }
 };
 
-
-
 export default {
   updateEnvState,
-    getEnvState
+  getEnvState,
 };
--- misc_mc/atiny85/light_test.c
+++ misc_mc/atiny85/light_test.c
@@ -1,56 +1,53 @@
 #include <stdint.h>
 
-#include <util/delay.h>
-
-#define F_CPU 8000000UL
-
 #define REG_OFFSET 0x20
 #define REG(x) (x + REG_OFFSET)
 
 #define PORTB *(volatile unsigned char *)REG(0x18)
 #define DDRB *(volatile unsigned char *)REG(0x17)
+// ADC registers
 #define ADMUX *(volatile unsigned char *)REG(0x07)
 #define ADCSRA *(volatile unsigned char *)REG(0x06)
 #define ADCSRB *(volatile unsigned char *)REG(0x03)
-
 #define ADCH *(volatile unsigned char *)REG(0x05)
 #define ADCL *(volatile unsigned char *)REG(0x04)
-#define DELAY 10000
+// Timer Refgisters
+// timer 0
+#define TCCR0A *(volatile unsigned char *)REG(0x2A)
+#define TCCR0B *(volatile unsigned char *)REG(0x33)
+#define TIFR *(volatile unsigned char *)REG(0x38)
+#define TCNT0 *(volatile unsigned char *)REG(0x32)
+// for pwm
+#define OCR0A *(volatile unsigned char *)REG(0x29)
+#define OCR0B *(volatile unsigned char *)REG(0x28)
 
 int main()
 {
-    DDRB |= (1 << 1);              // Set pin 1 as output
+
+    // Set pin 1 as output
+
+    DDRB |= (1 << 1);
+    // adc
     ADCSRA |= (1 << 7);            // enable adc
     ADMUX = 0x02;                  // Vcc as voltage ref, adc2 single ended input, right adjusted result
     ADCSRB = 0x00;                 // Free running mode
     ADCSRA |= (1 << 6) | (1 << 5); // Start ADC conversion and enabling Auto trigger
+    // timer-pwm
 
+    TCCR0A = 0x51; // fast pwm mode, non-inverting mode
+    TCCR0B = 0x00; // ensure timer stoped
+    TCCR0B = 0x09;
+    TCNT0 = 0x00; // clear timer counter
+    OCR0A = 255;
     while (1)
     {
         // start conversion
-        int adc_l = ADCL;
-        int trim_pot_value = (ADCH << 8) | adc_l;
+        uint16_t adc_l = ADCL;
+        uint16_t adc_h = ADCH;
+        uint16_t trim_pot_value = (adc_h << 8) | adc_l;
 
-        if (trim_pot_value > 512)
-        {
+        OCR0B = trim_pot_value / 4;
 
-            PORTB |= (1 << 1);
-            _delay_ms(1000);
-            // while (timer < DELAY)
-            // {
-            //     timer++;
-            // }
-            PORTB &= ~(1 << 1);
-            _delay_ms(1000);
-            // while (timer > 0)
-            // {
-            //     timer--;
-            // }
-        }
-        else
-        {
-            PORTB |= (1 << 1);
-        }
         ADCSRA |= (1 << 4); // interrupt flag
     }
 }
