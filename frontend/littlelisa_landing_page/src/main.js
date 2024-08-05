ce752c6b4ee992743828a6fe80f0d1a523ee8b7b
Cat Roman -Desktop <cjroman@lakeheadu.ca>
2024-08-05 14:50:18 -0700

    moving on for a bit

--- frontend/littlelisa_landing_page/src/main.js
+++ frontend/littlelisa_landing_page/src/main.js
@@ -6,18 +6,22 @@ import javascriptLogo from "./javascript.svg";
  */
 
 const scrollContainer = document.querySelector(".scroll-container");
+const sensorScene = document.querySelector(".sensor");
+const enviroScene = document.querySelector(".enviromental-control");
+const historyScene = document.querySelector(".history");
+const debugScene = document.querySelector(".debug");
 
 const startOfStickyContainer = document
   .querySelector(".scroll-container")
   .getBoundingClientRect().top;
 
-const endOfStickyContainer = startOfStickyContainer + window.innerHeight * 3;
+const endOfStickyContainer = startOfStickyContainer + window.innerHeight * 4;
 
 console.log(window.innerHeight);
 console.log(endOfStickyContainer);
 
 document.querySelector(".scroll-spacer").style.height =
-  window.innerHeight * 4 + "px";
+  window.innerHeight * 5 + "px";
 
 window.addEventListener("scroll", () => {
   if (
@@ -27,23 +31,82 @@ window.addEventListener("scroll", () => {
     console.log(window.scrollY);
 
     if (
-      //scene 1
       window.scrollY > startOfStickyContainer &&
-      window.scrollY < startOfStickyContainer + window.innerHeight
+      window.scrollY <= startOfStickyContainer + window.innerHeight
     ) {
-      console.log("scene 1");
-    } else if (
-      //scene 2
+      if (window.scrollY > startOfStickyContainer + window.innerHeight * 0.66) {
+        console.log("fade out scene 1");
+        //linear interpolation for transitions
+        // const fadeStart = startOfStickyContainer + window.innerHeight * 0.66;
+        // const fadeEnd = startOfStickyContainer + window.innerHeight;
+
+        // // Calculate normalized scroll position within the fade range
+        // const normalizedScroll =
+        //   (window.scrollY - fadeStart) / (fadeEnd - fadeStart);
+
+        // // Calculate opacity based on normalized scroll position
+        // const opacity = Math.max(1 - normalizedScroll, 0); // Ensures opacity does not go below 0
+
+        // // Apply calculated opacity
+        // sensorScene.style.opacity = opacity;
+      } else {
+        console.log("scene 1");
+        sensorScene.style.opacity = 1;
+        enviroScene.style.opacity = 0;
+        historyScene.style.opacity = 0;
+        debugScene.style.opacity = 0;
+      }
+    }
+    // Scene 2
+    else if (
       window.scrollY > startOfStickyContainer + window.innerHeight &&
-      window.scrollY < startOfStickyContainer + window.innerHeight * 2
+      window.scrollY <= startOfStickyContainer + window.innerHeight * 2
     ) {
-      console.log("scene 2");
-    } else if (
-      //scene 3
+      if (window.scrollY < startOfStickyContainer + window.innerHeight * 1.33) {
+        console.log("fade in scene 2");
+      } else if (scrollY > startOfStickyContainer + window.innerHeight * 1.66) {
+        console.log("fade out scene 2");
+      } else {
+        console.log("scene 2");
+        sensorScene.style.opacity = 0;
+        enviroScene.style.opacity = 1;
+        historyScene.style.opacity = 0;
+        debugScene.style.opacity = 0;
+      }
+    }
+    // Scene 3
+    else if (
       window.scrollY > startOfStickyContainer + window.innerHeight * 2 &&
-      window.scrollY < startOfStickyContainer + window.innerHeight * 3
+      window.scrollY <= startOfStickyContainer + window.innerHeight * 3
     ) {
-      console.log("scene 3");
+      if (window.scrollY < startOfStickyContainer + window.innerHeight * 2.33) {
+        console.log("fade in scene 3");
+      } else if (scrollY > startOfStickyContainer + window.innerHeight * 2.66) {
+        console.log("fade out scene 3");
+      } else {
+        console.log("scene 3");
+        sensorScene.style.opacity = 0;
+        enviroScene.style.opacity = 0;
+        historyScene.style.opacity = 1;
+        debugScene.style.opacity = 0;
+      }
+    }
+    // Scene 3
+    else if (
+      window.scrollY > startOfStickyContainer + window.innerHeight * 3 &&
+      window.scrollY <= startOfStickyContainer + window.innerHeight * 4
+    ) {
+      if (window.scrollY < startOfStickyContainer + window.innerHeight * 3.33) {
+        console.log("fade in scene 4");
+      } else if (scrollY > startOfStickyContainer + window.innerHeight * 3.66) {
+        console.log("fade out scene 4");
+      } else {
+        console.log("scene 4");
+        sensorScene.style.opacity = 0;
+        enviroScene.style.opacity = 0;
+        historyScene.style.opacity = 0;
+        debugScene.style.opacity = 1;
+      }
     }
     scrollContainer.style.position = "sticky";
     scrollContainer.style.top = "0px";
