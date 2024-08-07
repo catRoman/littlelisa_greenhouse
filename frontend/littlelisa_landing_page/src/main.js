323499f8f4f1d3fd0bb011acb58db0b28ee98ead
Cat Roman -Desktop <cjroman@lakeheadu.ca>
2024-08-06 21:18:15 -0700

    mostly done

--- frontend/littlelisa_landing_page/src/main.js
+++ frontend/littlelisa_landing_page/src/main.js
@@ -15,13 +15,13 @@ const startOfStickyContainer = document
   .querySelector(".scroll-container")
   .getBoundingClientRect().top;
 
-const endOfStickyContainer = startOfStickyContainer + window.innerHeight * 4;
+const endOfStickyContainer = startOfStickyContainer + window.innerHeight * 2;
 
 console.log(window.innerHeight);
 console.log(endOfStickyContainer);
 
 document.querySelector(".scroll-spacer").style.height =
-  window.innerHeight * 5 + "px";
+  window.innerHeight * 3 + "px";
 
 window.addEventListener("scroll", () => {
   if (
@@ -75,39 +75,7 @@ window.addEventListener("scroll", () => {
       }
     }
     // Scene 3
-    else if (
-      window.scrollY > startOfStickyContainer + window.innerHeight * 2 &&
-      window.scrollY <= startOfStickyContainer + window.innerHeight * 3
-    ) {
-      if (window.scrollY < startOfStickyContainer + window.innerHeight * 2.33) {
-        console.log("fade in scene 3");
-      } else if (scrollY > startOfStickyContainer + window.innerHeight * 2.66) {
-        console.log("fade out scene 3");
-      } else {
-        console.log("scene 3");
-        sensorScene.style.opacity = 0;
-        enviroScene.style.opacity = 0;
-        historyScene.style.opacity = 1;
-        debugScene.style.opacity = 0;
-      }
-    }
-    // Scene 3
-    else if (
-      window.scrollY > startOfStickyContainer + window.innerHeight * 3 &&
-      window.scrollY <= startOfStickyContainer + window.innerHeight * 4
-    ) {
-      if (window.scrollY < startOfStickyContainer + window.innerHeight * 3.33) {
-        console.log("fade in scene 4");
-      } else if (scrollY > startOfStickyContainer + window.innerHeight * 3.66) {
-        console.log("fade out scene 4");
-      } else {
-        console.log("scene 4");
-        sensorScene.style.opacity = 0;
-        enviroScene.style.opacity = 0;
-        historyScene.style.opacity = 0;
-        debugScene.style.opacity = 1;
-      }
-    }
+
     scrollContainer.style.position = "sticky";
     scrollContainer.style.top = "0px";
   } else if (window.scrollY > endOfStickyContainer) {
